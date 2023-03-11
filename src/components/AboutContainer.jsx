17ec1b1275f36785d3c7fa8b42a2abf8e21cbc34
EnioHenrique10 <enio_davi@hotmail.com>
2023-03-11 10:53:55 -0300

    commit inicial

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+    return (
+      <section className="about-container">
+        <h2>Sobre</h2>
+        <p>
+          Olá,sou desenvolvedor web, gosto muito de fazer sites web. 
+          Estou a procura de experiencia na ára de desenvolvimento web 
+       
+         
+        </p>
+      </section>
+    );
+  };
+  
+  export default AboutContainer;
